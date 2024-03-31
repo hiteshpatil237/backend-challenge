69c91f7630e36b3b086e180d38a190df51aa34cd
hiteshpatil237 <hiteshpatil237@gmail.com>
2024-03-31 19:02:45 -0400

    Added views and urls

--- tasks/application/views.py
+++ tasks/application/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import TaskSerializer, LabelSerializer
+from .models import Task, Label
 
-# Create your views here.
+class TaskViewSet(viewsets.ModelViewSet):
+    queryset = Task.objects.all().order_by('-created_at')
+    serializer_class = TaskSerializer
+
+class LabelViewSet(viewsets.ModelViewSet):
+    queryset = Label.objects.all().order_by('-created_at')
+    serializer_class = LabelSerializer
