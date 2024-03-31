cf3600daa15688268b22f4ab874bc4ccedfd981f
hiteshpatil237 <hiteshpatil237@gmail.com>
2024-03-31 16:28:53 -0400

    Created models

---
+++ tasks/application/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+class Label(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    owner = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+class Task(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    completion_status = models.BooleanField(default=False)
+    owner = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    labels = models.ManyToManyField(Label)
+
+    def __str__(self):
+        return self.title
